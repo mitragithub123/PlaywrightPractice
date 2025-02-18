b28a7aeb06f5303a80ebfb9424b2fd30d1ff3f71
Mitra bhanu <andolasoft.user133@.com>
2025-02-18 10:33:01 +0530

    Initial commit

---
+++ src/test/java/Demo.java
@@ -0,0 +1,15 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Demo {
+	public static void main(String[] args) {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Page page = browser.newPage();
+		page.navigate("https://www.orangescrum.com");
+		browser.close();
+		playwright.close();
+	}
+}
